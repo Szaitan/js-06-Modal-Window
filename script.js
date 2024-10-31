25111487f248fb3cb353d03293c15b1d83f6c874
Michal <m.krepiniewicz@gmail.com>
2024-10-31 21:34:42 +0100

    Useing for statment for querySelectorAll

--- script.js
+++ script.js
@@ -14,7 +14,19 @@
 
 // how it suppose to be done
 
-const modal = document.querySelector('.show-modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.style.display = '';
+  overlay.style.display = '';
+});
