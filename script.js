d40b6b2f30106eaed4d6e57b558352fae436d4be
Michal <m.krepiniewicz@gmail.com>
2024-11-01 13:10:12 +0100

    Classlist and Addevent for button press

--- script.js
+++ script.js
@@ -12,21 +12,46 @@
 //   document.querySelector('.overlay').style.display = 'none';
 // });
 
-// how it suppose to be done
+// how it suppose to be done with affecting the style.
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     modal.style.display = 'block';
+//     overlay.style.display = 'block';
+//   });
+// }
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.style.display = '';
+//   overlay.style.display = '';
+// });
+
+// How it should be done with affecting the classes of element aka using classlist
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.style.display = '';
-  overlay.style.display = '';
+function addHiddenfun() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', addHiddenfun);
+overlay.addEventListener('click', addHiddenfun);
+
+// AddEvent for specific button
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    console.log(e.key);
+    addHiddenfun();
+  }
 });
